d1da0d2385d740536b029beb8d2dedd7a75811c2
529486618@qq.com <fwhy226153>
2018-12-26 17:39:01 +0800

    python Error and Exception

--- Projects/Learning/Learning1/com/jieyi/PythonErrorAndException.py
+++ Projects/Learning/Learning1/com/jieyi/PythonErrorAndException.py
@@ -127,6 +127,95 @@
 
 ###5、用户自定义异常
 # 你可以通过创建一个新的异常类来拥有自己的异常。异常类继承自 Exception 类，可以直接继承，或者间接继承，例如:
+class MyError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+try:
+    raise MyError(2*2)
+except MyError as e:
+    print('My exception occurred, value:', e.value)
+
+# 输出：
+# My exception occurred, value: 4
+
+# raise MyError('oops!')
+#
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in ?
+# __main__.MyError: 'oops!'
+
+# 在这个例子中，类 Exception 默认的 __init__() 被覆盖。
+# 当创建一个模块有可能抛出多种不同的异常时，一种通常的做法是为这个包建立一个基础异常类，然后基于这个基础类为不同的错误情况创建不同的子类:
+
+
+###6、定义清理行为
+# try 语句还有另外一个可选的子句，它定义了无论在任何情况下都会执行的清理行为。 例如:
+# try:
+#     raise KeyboardInterrupt
+# finally:
+#     print('Goodbye, world!')
+#
+# 输出：
+# Goodbye, world!
+# Traceback (most recent call last):
+#   File "<stdin>", line 2, in <module>
+# KeyboardInterrupt
+
+# 以上例子不管 try 子句里面有没有发生异常，finally 子句都会执行。
+# 如果一个异常在 try 子句里（或者在 except 和 else 子句里）被抛出，而又没有任何的 except 把它截住，那么这个异常会在 finally 子句执行后再次被抛出。
+# 下面是一个更加复杂的例子（在同一个 try 语句里包含 except 和 finally 子句）:
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print("division by zero!")
+    else:
+        print("result is", result)
+    finally:
+        print("executing finally clause")
+
+# divide(2, 1)
+
+# 输出：
+# result is 2.0
+# executing finally clause
+
+# divide(2, 0)
+
+# 输出：
+# division by zero!
+# executing finally clause
+
+# divide("2", "1")
+
+# 输出：
+# executing finally clause
+
+
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in ?
+#   File "<stdin>", line 3, in divide
+# TypeError: unsupported operand type(s) for /: 'str' and 'str'
+
+
+###7、预定义的清理行为
+# 一些对象定义了标准的清理行为，无论系统是否成功的使用了它，一旦不需要它了，那么这个标准的清理行为就会执行。
+# 这面这个例子展示了尝试打开一个文件，然后把内容打印到屏幕上:
+# for line in open("myfile.txt"):
+#     print(line, end="")
+#
+# 以上这段代码的问题是，当执行完毕后，文件会保持打开状态，并没有被关闭。
+# 关键词 with 语句就可以保证诸如文件之类的对象在使用完之后一定会正确的执行他的清理方法:
+#
+# with open("myfile.txt") as f:
+#     for line in f:
+#         print(line, end="")
+# 以上这段代码执行完毕后，就算在处理过程中出问题了，文件 f 总是会关闭。
+
 
 
 
